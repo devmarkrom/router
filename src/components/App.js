387517db9bd2ebd4764490073a2a3fc15370e9e5
markozromero <marcosrrm15_v3@hotmail.com>
2019-04-28 15:06:54 -0500

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import Players from './Players';
-import Teams from './Teams';
-import Navbar from './Navbar';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Players from "./Players";
+import Teams from "./Teams";
+import Navbar from "./Navbar";
+import TeamPage from "./TeamPage";
+import Articles from "./Articles";
 
 class App extends Component {
   render() {
@@ -13,12 +15,15 @@ class App extends Component {
           <Navbar />
 
           <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/players' component={Players} />
-            <Route path='/teams' component={Teams} />
-            <Route render={() => <h1 className='text-center'>Four oh Four.</h1>} />
+            <Route path="/" exact component={Home} />
+            <Route path="/players" component={Players} />
+            <Route path="/teams" component={Teams} />
+            <Route path="/:teamId" exact component={TeamPage} />
+            <Route path="/:teamId/articles" component={Articles} />
+            <Route
+              render={() => <h1 className="text-center">Four oh Four.</h1>}
+            />
           </Switch>
-
         </div>
       </Router>
     );
